e0acbe065b014fae1887ae35efba342bd2bd1420
Aanchal Rana <aanchalrana@Aanchals-MacBook-Air.local>
2025-05-23 17:14:33 +0530

    new

---
+++ src/Introduction/IntroToSelenium.java
@@ -0,0 +1,23 @@
+package Introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class IntroToSelenium {
+
+	public static void main(String[] args) {
+		
+		// This is the program to introduce the object driver to implements the functions on the browsers
+		
+		
+		WebDriver driver = new ChromeDriver(); // Implements all the Web and Chrome Methods
+		
+		WebDriver driver1 = new FirefoxDriver();// Implements all Web and Firefox Methods
+		
+		
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SeleniumLearing {
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.firefox_driver;
+}
